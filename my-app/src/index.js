9f1b85a90715676511f733d5bd575e22007ed0e3
Ansh Sarkar <75054354+Ansh-Sarkar@users.noreply.github.com>
2020-11-26 08:43:44 +0530

    Add files via upload
    added my-app files

---
+++ my-app/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+let curDate = new Date(); /*year,month,day,hours : implicit declaration*/
+curDate = curDate.getHours();
+
+let greeting = '';
+const cssStyle = {};
+
+if(curDate>=1 && curDate<12){
+  greeting = 'Good Morning';
+  cssStyle.color = 'green';
+}
+else if(curDate>=12 && curDate<19){
+  greeting = 'Good Afternoon';
+  cssStyle.color = 'orange'; 
+}
+else{
+  greeting = 'Good Night';
+  cssStyle.color = 'black';
+}
+
+ReactDOM.render(
+<>
+<div>
+<h1> Hello Sir , <span style={cssStyle}> {greeting} </span></h1>
+</div>
+</>
+  ,document.getElementById('root')
+);
